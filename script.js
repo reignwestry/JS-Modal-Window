930f99e2254835d398246e90e2de22509c255218
Reign <reignzj.westry@gmail.com>
2021-10-27 00:11:17 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// 02 PROJECT: Modal Window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
+console.log(btnsOpenModal);
